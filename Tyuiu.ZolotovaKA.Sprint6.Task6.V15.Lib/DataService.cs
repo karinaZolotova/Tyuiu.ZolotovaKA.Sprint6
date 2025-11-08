f1c0d0ead997d301f7810c7eda136bd942e0f8c7
ZolotovaKarina <vse.prostit.utka@gmail.com>
2025-11-08 23:43:58 +0500

    Task6

--- Tyuiu.ZolotovaKA.Sprint6.Task6.V15.Lib/DataService.cs
+++ Tyuiu.ZolotovaKA.Sprint6.Task6.V15.Lib/DataService.cs
@@ -1,21 +1,26 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint6;
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint6;
 namespace Tyuiu.ZolotovaKA.Sprint6.Task6.V15.Lib
 {
     public class DataService : ISprint6Task6V15
     {
         public string CollectTextFromFile(string path)
         {
-            string resStr = "";
-            using (StreamReader reader = new StreamReader(path))
+            Console.WriteLine(path);
+            string[] lines = File.ReadAllLines(path);
+            StringBuilder result = new StringBuilder();
+            foreach (string line in lines)
             {
-                string line;
-                while ((line = reader.ReadLine()) != null)
+                string[] words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (words.Length > 0)
                 {
-                    string[] new_line = line.Split(' ');
-                    resStr += new_line.Last() + "";
+                    result.Append(words[^1] + " ");
                 }
             }
-            return resStr;
+
+            return result.ToString().Trim();
         }
     }
+
 }
