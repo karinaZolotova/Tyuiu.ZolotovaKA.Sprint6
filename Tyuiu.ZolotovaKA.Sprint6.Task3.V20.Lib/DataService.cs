5da4624a3d6f282fa59d6887ef4dfbbec10e094c
ZolotovaKarina <vse.prostit.utka@gmail.com>
2025-11-08 16:26:11 +0500

    Task3

--- Tyuiu.ZolotovaKA.Sprint6.Task3.V20.Lib/DataService.cs
+++ Tyuiu.ZolotovaKA.Sprint6.Task3.V20.Lib/DataService.cs
@@ -6,18 +6,24 @@ namespace Tyuiu.ZolotovaKA.Sprint6.Task3.V20.Lib
     {
         public int[,] Calculate(int[,] matrix)
         {
-            // Создаем копию исходной матрицы
             int rows = matrix.GetLength(0);
             int cols = matrix.GetLength(1);
+
+            // Создаем копию исходной матрицы
             int[,] result = new int[rows, cols];
 
-            // Копируем исходную матрицу
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < cols; j++)
                 {
                     result[i, j] = matrix[i, j];
                 }
+            }
+
+            // Заменяем последнюю строку на нули
+            for (int j = 0; j < cols; j++)
+            {
+                result[rows - 1, j] = 0;
             }
 
             return result;
